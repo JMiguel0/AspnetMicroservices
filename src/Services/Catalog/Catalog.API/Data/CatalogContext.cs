94c875e42cf1814cbb86fbae70a647b8cd901e9c
JMiguel <jarchundiah@toluca.tecnm.mx>
2022-07-01 14:57:15 -0500

    basket microservices and catalog microservices developed

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            //Mongo client connection
+            MongoClient client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            //Mongo databse connection
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            //Mongo collection connection
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            //Function to fill collection in Mongo if there´s no data
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        //Propertie where all products from collection are stored
+        public IMongoCollection<Product> Products { get; }
+    }
+}
